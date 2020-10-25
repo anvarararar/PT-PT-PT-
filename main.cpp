c316c8ede1e2166db2f38ad9ce83efd51d28d386
anvarararar <70436971+anvarararar@users.noreply.github.com>
2020-10-25 18:17:48 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,73 +2,109 @@
 #include <math.h>
 #include <assert.h>
 
-int kvadratnoe_uravnenie (double a, double b, double c);
-int linear_uravnenie(double b, double c);
+int kvadratnoe_uravnenie (double a, double b, double c, double* x1, double* x2, double* x);
+int linear_uravnenie (double b, double c, double* x);
+int vvod_chisel (double* a, double* b, double* c);
 
 int main()
 {
     printf ("Reshalka kvadratnogo uravnenia v.100500(c)Anvar\n");
 
-    printf ("Vvedi chisla a,b,c:\n");
-    double a, b, c;
-    int x = scanf("%lg %lg %lg", &a, &b, &c);
-    if (x!=3)
-        {
-        printf ("Do you know, chto takoe chislo? Yes? Tak pochemu je you vvodish kakuyto dich?!?!!!\nZapusti programmu zanovo i vvedi chisla!\n");
-        return -1;
-        }
+    double x1 = NAN;
+    double x2 = NAN;
+    double x = NAN;
+    double a = NAN;
+    double b = NAN;
+    double c = NAN;
 
-    kvadratnoe_uravnenie (a, b, c);
+    int proverka = vvod_chisel (&a, &b, &c);
+
+    if (proverka == 4)
+        return 0;
+
+    int kolichestvo_korney = kvadratnoe_uravnenie (a, b, c, &x1, &x2, &x);
+
+    if (kolichestvo_korney == 2)
+        printf ("dva kornya\n    x1 = %lg\n    x2 = %lg\n", x1, x2);
+
+    else if (kolichestvo_korney == 1)
+         printf ("odin koren\n   x = %lg\n", x);
+
+    else if (kolichestvo_korney == -1)
+        printf ("beskonechnoe kolichestvo korney\n");
+
+    else if (kolichestvo_korney == 0)
+        printf ("korney net\n");
+
     return 0;
 }
 
 //-----------------------------------------------------------------------------
 
-int kvadratnoe_uravnenie (double a, double b, double c)
+int kvadratnoe_uravnenie (double a, double b, double c, double* x1, double* x2, double* x)
 {
-    double discr = b*b-4*a*c;
+    double discr = (b*b-4*a*c);
 
     if (a == 0)
-        linear_uravnenie(b, c);
+        return linear_uravnenie(b, c, x);
 
     else if      (discr > 0)
     {
-        double x1=(-b+sqrt(b*b-4*a*c))/2/a;
-        double x2=(-b-sqrt(b*b-4*a*c))/2/a;
-        printf ("dva kornya\n    x1=%lg\n    x2=%lg\n",x1,x2);
+        double t = sqrt(discr);// корень из дискриминанта
+        *x1 = ((-b+t))/2/a;
+        *x2 = (-b-t)/2/a;
+        return 2;
     }
 
     else if (discr < 0)
-        printf ("Net korney");
+        return 0;
 
-    else if (discr = 0)
-        printf ("odin koren\n   x=%lg\n",(-b)/(2*a));
+    else if (discr == 0)
+    {
+        *x = (-b)/(2*a);
+        return 1;
+    }
 
-    return 0;
+    return 3;
 }
 
 
 //-----------------------------------------------------------------------------
 
-int linear_uravnenie (double a, double b)
+int linear_uravnenie (double a, double b, double* x)
 {
     if (a!=0)
     {
-        double x=((-b)/a);
-        printf ("odin koren\n   x=%lf\n",x);
+        *x = ((-b)/a);
+        return 1;
     }
 
     if ((a==0) && (b==0))
-        printf ("beskonechnoe kolichestvo korney\n");
-
+        return -1;
 
     if ((a==0) && (b!=0))
-        printf ("korney net\n");
+        return 0;
+
 
     printf ("\nPT!PT!PT!\n");
 
 }
 
+
+//-----------------------------------------------------------------------------
+
+int vvod_chisel (double* a, double* b, double* c)
+{
+    printf ("Vvedi chisla a,b,c:\n");
+    int y = scanf("%lg %lg %lg", a, b, c);
+    if (y != 3)
+        {
+            printf ("Do you know, chto takoe chislo? Yes?"
+            "Tak pochemu je you vvodish kakuyto dich?!?!!!\n"
+            "Zapusti programmu zanovo i vvedi chisla!\n");
+            return 4;
+        }
+}
 
 
 
